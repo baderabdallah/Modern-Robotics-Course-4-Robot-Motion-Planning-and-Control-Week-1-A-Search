c44e5c3162ddffaa6dced32c6bd7262a35316689
abdallah <bader.abdallah@outlook.com>
2020-09-27 14:55:15 +0200

    Nodes list is given a proper structure

--- Astar.py
+++ Astar.py
@@ -2,6 +2,12 @@ import numpy as np
 from numpy import genfromtxt
 import os
 
+class node:
+    def __init__(self, order, x, y, heuristicCostToGo):
+        self.order = int(order)
+        self.x = float(x)
+        self.y = float(y)
+        self.heuristicCostToGo = float(heuristicCostToGo)
 
 nodesFileName = os.getcwdb()
 nodesFileName = nodesFileName.decode("utf-8")+u'\\Scene5_example\\nodes.csv'
@@ -13,5 +19,11 @@ edgesFileName = edgesFileName.decode("utf-8")+u'\\Scene5_example\\edges.csv'
 
 edges = genfromtxt(edgesFileName, delimiter=',')
 
-print (nodes)
-print (edges)
+nodeList = list()
+for eachNode in nodes:
+    n = node(eachNode[0],eachNode[1],eachNode[2],eachNode[3])
+    nodeList.append(n)
+
+
+for i in nodeList:
+    print(i.order, i.x, i.y, i.heuristicCostToGo)
