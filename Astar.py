0c5514507aac1575e215dddb54ed6e4a9464b427
abdallah <bader.abdallah@outlook.com>
2020-10-09 21:17:34 +0200

    An edge class is created along with its relevant members.
    Parsed edge data is transformed into a list of edge objects.

--- Astar.py
+++ Astar.py
@@ -11,19 +11,27 @@ class node:
 
 nodesFileName = os.getcwdb()
 nodesFileName = nodesFileName.decode("utf-8")+u'\\Scene5_example\\nodes.csv'
-
 nodes = genfromtxt(nodesFileName, delimiter=',')
-
-edgesFileName = os.getcwdb()
-edgesFileName = edgesFileName.decode("utf-8")+u'\\Scene5_example\\edges.csv'
-
-edges = genfromtxt(edgesFileName, delimiter=',')
 
 nodeList = list()
 for eachNode in nodes:
     n = node(eachNode[0],eachNode[1],eachNode[2],eachNode[3])
     nodeList.append(n)
 
+class edge: 
+    def __init__(self, childNodeId, parentNodeId, cost):
+        self.childNodeId = int(childNodeId)
+        self.parentNodeId = int(parentNodeId)
+        self.cost = float(cost)
+
+edgesFileName = os.getcwdb()
+edgesFileName = edgesFileName.decode("utf-8")+u'\\Scene5_example\\edges.csv'
+edges = genfromtxt(edgesFileName, delimiter=',')
+
+edgeList = list()
+for eachEdge in edges:
+    m = edge(eachEdge[0], eachEdge[1], eachEdge[2])
+    edgeList.append(m)
 
 for i in nodeList:
     print(i.order, i.x, i.y, i.heuristicCostToGo)
